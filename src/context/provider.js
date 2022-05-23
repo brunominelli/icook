b6d78bacc5406b21075b68447fc07873d29ebf11
Bruno Minelli <minellibruno@hotmail.com>
2022-05-23 12:23:05 -0300

    Consulta à API's e gerenciamento de estado em desenvolvimento.

--- src/context/provider.js
+++ src/context/provider.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchDrinks, fetchMeals } from '../services/api';
+import Context from './context';
 
-function Provider() {
+function Provider({ children }) {
+  const [meals, setMeals] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  const user = { username: '', email: '', password: '' };
+  const recipes = {
+    id: '',
+    type: '',
+    nationality: '',
+    category: '',
+    alcoholicOrNot: '',
+    name: '',
+    image: '',
+    doneDate: '',
+    tags: '',
+  };
+
+  const setStorage = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('drinksToken', 1);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('inProgressRecipes', []);
+    localStorage.setItem('doneRecipes', JSON.stringify([recipes]));
+    localStorage.setItem('favoriteRecipes', JSON.stringify([recipes]));
+  }
+
+  useState( async () => {
+    const mealsAPI = await fetchMeals('');
+    const drinksAPI = await fetchDrinks('');
+    setMeals(mealsAPI);
+    setDrinks(drinksAPI);
+    setStorage();
+  }, [])
+
+  const contextValue = {
+    meals,
+    drinks,
+  }
+
   return (
-    <div>Provider</div>
+    <Context.Provider value={ contextValue }>
+      {children}
+    </Context.Provider>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,15 @@
-const fetchMeals = () => {};
-const fetchDrinks = () => {};
+const fetchMeals = async (query) => {
+  const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return response.ok ? Promise.resolve(data.meals) : Promise.reject(data);
+};
+
+const fetchDrinks = async (query) => {
+  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return response.ok ? Promise.resolve(data.drinks) : Promise.reject(data);
+};
 
 export { fetchMeals, fetchDrinks };
