9952f1cde8523c69d1ef24dcbc8cec01dc6db33d
Bruno Minelli <minellibruno@hotmail.com>
2022-06-07 17:07:34 -0300

    Feat: Filtro de bebidas por tipo de copo.

--- src/pages/explore/ExploreCocktailsGlasses.jsx
+++ src/pages/explore/ExploreCocktailsGlasses.jsx
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppCard from '../../components/AppCard';
 import AppFooter from '../../components/AppFooter';
 import AppHeader from '../../components/AppHeader';
 import Context from '../../context/context';
+import { fetchCocktails } from '../../services/api';
 import { Block, Container, Form, Select, Wrapper } from '../../styles';
 
 function ExploreCocktailsGlasses() {
-  const { drinks, drinksGlasses } = useContext(Context);
-  console.log(drinksGlasses);
+  const { cocktails, cocktailsGlasses } = useContext(Context);
+  
+  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState([]);
+
+  useEffect(() => {
+    const getSearch = async () => {
+      const recipes = await fetchCocktails(query, 'glass');
+      setSearch(recipes);
+    }
+    getSearch();
+  }, [query]);
+
   return (
       <>
       <AppHeader />
       <Container>
         <Wrapper>
         <Form>
-            <Select>
+            <Select onChange={ (e) => setQuery(e.target.value) }>
               <option value=''>All</option>
-              {drinksGlasses.map((glass, index) =>
+              {cocktailsGlasses.map((glass, index) =>
                 <option
                   key={index}
                   value={glass.strGlass}>
@@ -26,15 +38,24 @@ function ExploreCocktailsGlasses() {
             </Select>
           </Form>
           <Block>
-            {drinks.map((drink, index) =>
-              index < 24 && <AppCard
-                key={ drink.idDrink }
-                id={ drink.idDrink }
-                title={ drink.strDrink }
-                thumb={ drink.strDrinkThumb }
-                type='drinks'
-              />
-            )}
+            {(!query && cocktails)
+              ? cocktails.map((cocktail, index) =>
+                index < 24 && <AppCard
+                  key={ cocktail.idDrink }
+                  id={ cocktail.idDrink }
+                  title={ cocktail.strDrink }
+                  thumb={ cocktail.strDrinkThumb }
+                  type='cocktails'
+                />
+              ): (query && search) && search.map((recipe) =>
+                <AppCard
+                  key={ recipe.idDrink }
+                  id={ recipe.idDrink }
+                  title={ recipe.strDrink }
+                  thumb={ recipe.strDrinkThumb }
+                  type='cocktails'
+                />
+              )}
           </Block>
         </Wrapper>
       </Container>
--- src/services/api.js
+++ src/services/api.js
@@ -41,8 +41,23 @@ const fetchRandomMeal = async () => {
   return response.ok ? Promise.resolve(data.meals) : Promise.reject(data);
 };
 
-const fetchCocktails = async (query) => {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
+const fetchCocktails = async (query, searchBy) => {
+  let endpoint = '';
+  switch (searchBy) {
+    case 'ingredient': 
+      endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}`;
+      break;
+    case 'category':
+      endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${query}`;
+      break;
+    case 'glass':
+      endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${query}`;
+      break;
+    default:
+      endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
+      break;
+  }
+
   const response = await fetch(endpoint);
   const data = await response.json();
   return response.ok ? Promise.resolve(data.drinks) : Promise.reject(data);
