f508b2a0c6e4784c52fb8ba887c613bfd8e2b7c2
Bruno Minelli <minellibruno@hotmail.com>
2022-06-07 16:42:24 -0300

    Feat: Filtro de comidas por nacionalidade

--- src/pages/explore/ExploreMealsNacionalities.jsx
+++ src/pages/explore/ExploreMealsNacionalities.jsx
@@ -1,12 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppCard from '../../components/AppCard';
 import AppFooter from '../../components/AppFooter';
 import AppHeader from '../../components/AppHeader';
 import Context from '../../context/context';
+import { fetchMeals } from '../../services/api';
 import { Block, Container, Form, Select, Wrapper } from '../../styles';
 
 function ExploreMealsNacionalities() {
   const { meals, mealsNacionalities } = useContext(Context);
+
+  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState([]);
+
+  useEffect(() => {
+    const getSearch = async () => {
+      const recipe = await fetchMeals(query, 'nacionality');
+      setSearch(recipe);
+    }
+    getSearch();
+  }, [query]);
+
 
   return (
     <>
@@ -14,7 +27,7 @@ function ExploreMealsNacionalities() {
       <Container>
         <Wrapper>
           <Form>
-            <Select>
+            <Select onChange={ (e) => setQuery(e.target.value) }>
               <option value=''>All</option>
               {mealsNacionalities.map((nacionaty, index) =>
                 <option
@@ -26,15 +39,24 @@ function ExploreMealsNacionalities() {
             </Select>
           </Form>
           <Block>
-            {meals.map((meal, index) =>
-              index < 24 && <AppCard
-                key={ meal.idMeal }
-                id={ meal.idMeal }
-                title={ meal.strMeal }
-                thumb={ meal.strMealThumb }
-                type='meals'
-              />
-            )}
+            {(!query && meals)
+              ? meals.map((meal, index) =>
+                index < 24 && <AppCard
+                  key={ meal.idMeal }
+                  id={ meal.idMeal }
+                  title={ meal.strMeal }
+                  thumb={ meal.strMealThumb }
+                  type='meals'
+                />
+              ): (query && search) && search.map((recipe) =>
+                <AppCard
+                  key={ recipe.idMeal }
+                  id={ recipe.idMeal }
+                  title={ recipe.strMeal }
+                  thumb={ recipe.strMealThumb }
+                  type='meals'
+                />
+              )}
           </Block>
         </Wrapper>
       </Container>
